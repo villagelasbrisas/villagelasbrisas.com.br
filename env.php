12c5af100bad940ed4cd668c036b13ec7d930efc
gcairesdev <pessoalgcaires@gmail.com>
2020-05-10 20:02:08 -0300

    fix: url production variable fixed

--- env.php
+++ env.php
@@ -2,10 +2,8 @@
 
     if ($_SERVER["SERVER_NAME"] == "localhost") {
         $env_url = "http://localhost/villagelasbrisas.com.br/";
-    } elseif ($_SERVER["SERVER_NAME"] == "villagelasbrisas") {
-        $env_url = "https://www.villagelasbrisas.com.br/";
     } else {
-        $env_url = "http://www.dev.villagelasbrisas.com.br/";
+        $env_url = "https://www.villagelasbrisas.com.br/";
     }
 
     define('INCLUDE_PATH',$env_url);
