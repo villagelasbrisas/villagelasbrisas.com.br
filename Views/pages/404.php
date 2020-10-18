4b370df23cc8054cde497b2bffa5c343f005d42e
gcairesdev <pessoalgcaires@gmail.com>
2020-10-18 16:11:58 -0300

    :lipstick: Changed 404 page

--- Views/pages/404.php
+++ Views/pages/404.php
@@ -1,6 +1,6 @@
-<div id="page-404">
+<div id="page-404" data-aos="fade-up" data-aos-duration="800">
     <main>
-        <h1>404</h1>
+        <img src="<?= INCLUDE_PATH_FULL ?>/img/icons/404.svg" alt="404">
         <p>Ops! Página não encontrada.</p>
         <a href="<?= INCLUDE_PATH ?>">Voltar ao site</a>
     </main>
