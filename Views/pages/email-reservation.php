42aabb0fc2a029dce9d1c712780337507108450f
gcairesdev <pessoalgcaires@gmail.com>
2020-10-25 20:23:20 -0300

    :rocket: Added new reservation e-mail

--- Views/pages/email-reservation.php
+++ Views/pages/email-reservation.php
@@ -1,102 +1,142 @@
 <?php
 
-$emailReservationPage = "
-    <html>
-        <head>
-            <style>
-                html, body { width: 100%; height: 100%;}
-                html, body, div, span, h1, h2, h3, h4, h5, h6, p,
-                a, img, s, strong, sub, sup, b, u, i, center,ol, ul, li,
-                form, label, legend, table, tbody, tfoot, thead, tr, th, td,
-                article, aside, footer, header, menu, nav, section
-                {margin: 0;padding: 0;border: 0;font-size: 100%;font: inherit;vertical-align: baseline;}
-                footer, header, nav, section {display: block;}
-                body {line-height: 1;}
-                ol, ul {list-style: none;}
-                table {border-collapse: collapse;border-spacing: 0;}
+$emailReservationPage = '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>E-mail de reserva</title>
+</head>
+<body style="margin: 0; padding: 0;">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse; max-width: 600px;">
+        <tr>
+            <td align="center" bgcolor="#00AA88" style="padding: 30px 0;">
+                <img src="http://www.villagelasbrisas.com.br/Views/pages/img/logo-contorno.png" alt="Logo Las Brisas" style="display: block;" />
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td style="color: #153643; font-family: Arial, sans-serif; font-size: 22px;">
+                            <b>E-mail reserva - ' . $name . '</b>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 18px;">
+                            ' . $message . '
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td width="50%" valign="top" style="padding: 25px 0;">
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td align="center">
+                                                    <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/adults.png" alt="Check-In" width="57" height="57" style="display: block;" />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 10px; color: #00AACC; font-family: Arial, sans-serif; font-size: 21px;">
+                                                    <b>' . $adults . '</b>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td width="50%" valign="top" style="padding: 25px 0;">
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td align="center">
+                                                    <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/baby.png" alt="Check-Out" width="57" height="57" style="display: block;" />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 10px; color: #00AACC; font-family: Arial, sans-serif; font-size: 21px;">
+                                                    <b>' . $kids . '</b>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td width="50%" valign="top" style="padding: 25px 0;">
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td align="center">
+                                                    <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/calendar.png" alt="Check-In" width="50" height="50" style="display: block;" />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 10px; color: #00AACC; font-family: Arial, sans-serif; font-size: 18px;">
+                                                    <b>' . $checkin . '</b>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                    <td width="50%" valign="top" style="padding: 25px 0;">
+                                        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                            <tr>
+                                                <td align="center">
+                                                    <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/calendar.png" alt="Check-Out" width="50" height="50" style="display: block;" />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 10px; color: #00AACC; font-family: Arial, sans-serif; font-size: 18px;">
+                                                    <b>' . $checkout . '</b>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td bgcolor="#FF9035" style="padding: 30px 30px 30px 30px;">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                    <tr>
+                        <td align="right">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td style="color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;" width="75%">
+                                        &reg; Village Las Brisas, Vera Cruz - BA 2020 <br/>
 
-                main {
-                    width: 100%;
-                    height: 100%;
-                    margin: auto;
-                    color: #121212;
-                    font-weight: bolder;
-                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                }
+                                        <a href="#" style="color: #ffffff;">
+                                            <font color="#ffffff">
+                                                Remova sua inscrição
+                                            </font>
+                                        </a>
 
-                header {
-                    width: 100%;
-                    margin: 25px auto;
-                }
-
-                header img {
-                    margin: 0 auto;
-                }
-
-                h1 {
-                    font-size: 18pt;
-                    margin-top: 30px;
-                    text-align: center;
-                    text-transform: uppercase;
-                }
-
-                h2 {
-                    font-size: 16pt;
-                    text-align: center;
-                    color: #10AC84;
-                    margin-bottom: 15px;
-                }
-
-                ul {
-                    margin: 20px auto;
-                }
-
-                ul li {
-                    margin: 15px;
-                    color: #10AC84;
-                    font-size: 15pt;
-                    text-align: left:
-                }
-
-                ul li span {
-                    color: #121212;
-                }
-
-                p {
-                    width: 90%;
-                    margin: 0 auto;
-                    font-size: 14pt;
-                    max-width: 250px;
-                    line-height: 1.3;
-                    text-align: justify;
-                }
-            </style>
-        </head>
-        <body>
-            <main>
-                <header>
-                    <img src=\"http://villagelasbrisas.com.br/Views/pages/img/logo.png\" alt=\"Logo Las Brisas\" align=\"center\">
-                    <h1>Reserva Las Brisas</h1>
-                </header>
-
-                <section>
-                    <h2>$name</h2>
-
-                    <div align=\"center\" style=\"text-align: center;\">
-                        <ul>
-                            <li><span>Adultos..:</span> $adults</li>
-                            <li><span>Crianças:</span> $kids</li>
-                            <li><span>Entrada..:</span> $checkin</li>
-                            <li><span>Saída......:</span> $checkout</li>
-                        </ul>
-                    </div>
-
-                    <p>$message</p>
-                </section>
-            </main>
-        </body>
-    </html>
-";
+                                        desse e-mail marketing, instantaneamente.
+                                    </td>
+                                    <td>
+                                        <a href="https://www.facebook.com/villagelasbrisas" target="_blank">
+                                            <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/facebook.png" alt="Facebook" width="38" height="38" style="display: block;" border="0" />
+                                        </a>
+                                    </td>
+                                    <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                                    <td>
+                                        <a href="https://www.instagram.com/villagelasbrisas" target="_blank">
+                                            <img src="https://www.villagelasbrisas.com.br/Views/pages/img/icons/instagram.png" alt="Instagram" width="32" height="32" style="display: block;" border="0" />
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+';
 
 return $emailReservationPage;
 
