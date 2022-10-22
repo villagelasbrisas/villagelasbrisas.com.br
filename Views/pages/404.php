bf190e9ea9ee82d36ed3b7cdb2ad438f5b183a4c
Guilherme Caires <contatogcaires@gmail.com>
2022-10-22 10:51:25 -0300

    PHP TO HTML

--- Views/pages/404.php
+++
@@ -1,7 +0,0 @@
-<div id="page-404">
-    <main>
-        <img src="<?= INCLUDE_PATH_FULL ?>/img/icons/404.svg" alt="404">
-        <p>Ops! Página não encontrada.</p>
-        <a href="<?= INCLUDE_PATH ?>">Voltar ao site</a>
-    </main>
-</div>
